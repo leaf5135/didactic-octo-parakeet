196ee76a2fb9088fa9f54ba64ec8c1a091be52f1
Andrew <andiep.sf@gmail.com>
2025-09-13 12:35:27 -0700

    Create todo.ts

---
+++ convex-todo/convex/todo.ts
@@ -0,0 +1,24 @@
+import { mutation, query } from "./_generated/server";
+import { v } from "convex/values";
+
+export const createTodo = mutation({
+  args: { text: v.string() },
+  handler: async (ctx, args) => {
+    await ctx.db.insert("todos", { text: args.text, completed: false });
+  },
+});
+
+export const getTodos = query({
+  handler: async (ctx) => {
+    return await ctx.db.query("todos").collect();
+  },
+});
+
+export const toggleTodo = mutation({
+  args: { id: v.id("todos") },
+  handler: async (ctx, args) => {
+    const todo = await ctx.db.get(args.id);
+    if (!todo) return;
+    await ctx.db.patch(args.id, { completed: !todo.completed });
+  },
+});
